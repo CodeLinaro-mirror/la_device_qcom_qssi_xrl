05ab5b5a5ccf4b0656e8b10ecbe81c1915a9d543
LAVEENA FULWANI <quic_lfulwani@quicinc.com>
2024-08-26 12:37:32 +0530

    qssi_xrl lunch creation
    Change-Id: I4c4e3b6e4655433db18b379c601b65f6dd8488bc

---
+++ target_specific_configs.py
@@ -0,0 +1,5 @@
+#Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
+#SPDX-License-Identifier: BSD-3-Clause-Clear
+
+target_deprecate_kernel_headers = ["qssi_xrl", "seraph"]
+target_deprecate_target_out_headers = ["qssi_xrl", "seraph"]
